fead3a8158945ca2474a506046333c5f98579ba3
ren <royelreine@gmail.com>
2026-07-10 10:06:40 -0700

    password genrator simple and difficult both code for understanding

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,18 @@
+print(range(1, 10))
+for number in range(1, 10):
+    print(number)
+
+for number in range(1, 11):
+    print(number)
+
+
+# simple direct add
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
+total = 0
+for number in range(1, 10):
+    total += number
+print(total)
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,22 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+#student_scores_1 = [150, -142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+student_scores = [8 ,65, 89, 86, 55 ,91, 64, 89]
 print(range(1, 10))
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(f"max score=+ {max_score}")
+
+sum = 0
+for score in student_scores:
+    sum += score
+
+print(f"sum={sum}")
+
+min_score = 0
+for score in student_scores:
+    if score < min_score:
+        min_score = score
+
+print(f"min score={min_score}")
