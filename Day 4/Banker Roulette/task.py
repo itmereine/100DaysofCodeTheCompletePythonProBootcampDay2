93870b14bddf84fb6b2ca6e8ba2002862b5b0588
ren <royelreine@gmail.com>
2026-07-10 07:08:16 -0700

    Rock,Paper,Scissor game

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,12 @@
+#friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+#print("Alice")
+
+import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# 1st Option
+print(random.choice(friends))
+
+# 2nd Option
+random_index = random.randint(0, 4)
+print(friends[random_index])
